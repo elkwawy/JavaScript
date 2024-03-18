cf8397f70999130e0f49b02e1b61aca14c7970ba
unknown <ama991969@gmail.com>
2024-03-18 09:59:24 +0200

    Assignments Javascript Elzero

---
+++ Assignments Javascript Elzero/From 18 to 22 [Operators]/Assignment 4.js
@@ -0,0 +1,16 @@
+// Assignment 4
+let points = 10;
+
+// Write Your Code Here
+++points;
+++points;
+++points;
+console.log(points); // 13
+
+// Write Your Code Here
+--points;
+--points;
+--points;
+--points;
+--points;
+console.log(points); // 8;
